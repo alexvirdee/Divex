4c2cb4722d0c0715aaa029da5a4d05c1244d3428
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-21 09:40:44 -0500

    remove

--- server/divex/app.js
+++ server/divex/app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+// LINK ROUTES
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// MIDDLEWARE
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
