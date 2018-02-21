cb5e0fa4bc6241a3033ade47a90f5ed13c23e7cb
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-21 11:17:05 -0500

    boilerplate

--- client/divex/src/components/SignUpForm.js
+++ client/divex/src/components/SignUpForm.js
@@ -6,8 +6,26 @@ export default class SignUpForm extends Component {
 		return (
 				<div className="SignUpForm">
 				<h2> Sign up to Divex </h2>
+				<form id="form-container">
+  <label for="username">Username</label>
+  <input id="username" type="text" name="username" >
+  <br><br>
+  <label for="password">Password</label>
+  <input placeholder="Your password">
 
-				
+  <br><br>
+
+  <button>Create account</button>
+
+  <p>
+    Do you already have an account?
+    <a href="#">Login</a>
+  </p>
+
+  <p>
+    <a href="/logout">Logout</a>
+  </p>
+</form>
 			)
 	}
 }
