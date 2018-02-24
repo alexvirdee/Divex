874881bba5819c4b2fab17a4210b454efdac4ed2
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-24 16:57:33 -0500

    background home page setip

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passportRouter = require('./routes/auth');
 var router = require('./routes/api');
+var expressLayouts = require('express-ejs-layouts');
 
 
 // Mongoose Configure
