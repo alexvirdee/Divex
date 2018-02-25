c2d0f3ff16e27a09e8c8d8193832ef3de51a50b1
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-25 14:06:36 -0500

    geojson

--- models/dive.js
+++ models/dive.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+"geometry": {
+    "type": "Point",
+    "coordinates": [125.6, 10.1]
+  }
+
+// create geolocation Schema
+const GeoSchema = new Schema({
+	type: {
+		type: String,
+		default: "Point"
+	},
+	coordinates: {
+		type: [Number],
+		index: "2dsphere"
+	}
+});
+
 // create dive Schema & model
 const DiveSchema = new Schema({
 	number: {
@@ -16,7 +33,7 @@ const DiveSchema = new Schema({
 	visibility: Number,
 	totalTime: Number,
 	depth: Number,
-	observations: String
+	observations: [String]
 
 	// add in images to Schema
 
