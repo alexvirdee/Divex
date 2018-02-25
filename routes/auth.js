80665626cd12ea3a48312a344f5c4663e306c558
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-24 20:28:12 -0500

    login and signup view

--- routes/auth.js
+++ routes/auth.js
@@ -7,59 +7,63 @@ const bcryptSalt = 10;
 const ensureLogin = require('connect-ensure-login');
 const passport = require('passport');
 
-// GET request to signup form
-passportRouter.get('/signup', (req, res, next) => {
-
-});
-
-// Signup a new user
-passportRouter.post('/signup', (req, res, next) => {
-	const username = req.body.username;
-	const password = req.body.password;
-
-	if (username === "" || password === "") {
-		// do something
-	}
-
-	User.findOne({ username }, "username", (err, user) => {
-		if (user !== null) {
-			// do something
-			return;
-		}
-
-		const salt = bcrypt.genSaltSync(bcryptSalt);
-		const hashPass = bcrypt.hashSync(password, salt);
-
-		const newUser = new User({
-			username,
-			password: hashPass
-		});
-
-		newUser.save((err) => {
-			if (err) {
-				// do something
-			} else {
-				res.redirect("/");
-			}
-		});
-	});
-});
 
 
 passportRouter.get('/login', (req, res, next) => {
-	// do something
+	res.render('auth/login');
 });
 
-passportRouter.post('/login', passport.authenticate("local", {
-	successRedirect: "/",
-	failureRedirect: "/login",
-	failureFlash: true,
-	passReqToCallback: true
-}));
+// passportRouter.post('/login', passport.authenticate("local", {
+// 	successRedirect: "/",
+// 	failureRedirect: "/login",
+// 	failureFlash: true,
+// 	passReqToCallback: true
+// }));
 
-passportRouter.get("/logout", (req, res, next) => {
-	res.logout();
-	res.redirect("/login");
+// GET request to signup form
+passportRouter.get('/signup', (req, res, next) => {
+	res.render('auth/signup');
 });
+
+// Signup a new user
+// passportRouter.post('/signup', (req, res, next) => {
+// 	const username = req.body.username;
+// 	const password = req.body.password;
+
+// 	if (username === "" || password === "") {
+// 		// do something
+// 	}
+
+// 	User.findOne({ username }, "username", (err, user) => {
+// 		if (user !== null) {
+// 			// do something
+// 			return;
+// 		}
+
+// 		const salt = bcrypt.genSaltSync(bcryptSalt);
+// 		const hashPass = bcrypt.hashSync(password, salt);
+
+// 		const newUser = new User({
+// 			username,
+// 			password: hashPass
+// 		});
+
+// 		newUser.save((err) => {
+// 			if (err) {
+// 				// do something
+// 			} else {
+// 				res.redirect("/");
+// 			}
+// 		});
+// 	});
+// });
+
+
+
+
+// passportRouter.get("/logout", (req, res, next) => {
+// 	res.logout();
+// 	res.redirect("/login");
+// });
 
 module.exports = passportRouter;
